327281abd3eefeea5ffd189732626a78a18a1c1b
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2022-09-20 16:56:18 +0900

    01

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int input_int;
+	float input_float;
+	
+	printf("정수를 입력하시오: ");
+	scanf("%d", &input_int);
+	
+	printf("소수를 입력하시오: ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float); 
+	return 0;
+}
